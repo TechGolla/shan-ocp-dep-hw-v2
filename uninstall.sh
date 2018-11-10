957908a98232136b62939bfc2988ad4cce64a0f1
TechGolla <techgolla@gmail.com>
2018-11-10 17:37:53 -0500

    updates to uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -4,10 +4,6 @@ echo 'Uninstalling Openshift'
 echo 'set log_plays to true'
 export ANSIBLE_LOG_PATH=/var/log/ansible_uninstall.log
 
-echo '*******************SET GUID********************'
-export GUID=`hostname | cut -d"." -f2`; echo "export GUID=$GUID" >> $HOME/.bashrc
-echo 'GUID ==> '$GUID
-
 echo '*********RUN UNINSTALL PLAYBOOK****************'
 echo '/usr/share/ansible/openshift-ansible/playbooks/adhoc/uninstall.yml'
 ansible-playbook /usr/share/ansible/openshift-ansible/playbooks/adhoc/uninstall.yml
