1258d138432a3ec078a430d6e94f3d5dab744601
TechGolla <techgolla@gmail.com>
2018-11-15 20:23:38 -0500

    adhoc commands

---
+++ adhoc-commands.sh
@@ -0,0 +1,16 @@
+#Create new project CI/CD to host all pipeline tools
+oc new-project cicd --display-name="CI/CD"
+#Create new projects for DEV and STage
+oc new-project dev --display-name="Tasks - Dev"
+oc new-project stage --display-name="Tasks - Stage"
+
+#Add Permissions for cicd for DEV and STAGE
+oc policy add-role-to-group edit system:serviceaccounts:cicd -n dev
+oc policy add-role-to-group edit system:serviceaccounts:cicd -n stage
+
+#Build cicd project with all pipeline tools
+oc new-app -n cicd -f cicd-template.yaml --param=DEPLOY_CHE=true
+
+#To start the build pipeline
+#oc start-build <pipelineName>
+oc start-build tasks-pipeline
