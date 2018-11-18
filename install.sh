6222c7b4689a7476ca5266a4d3384472d13ee3e0
TechGolla <techgolla@gmail.com>
2018-11-18 10:28:25 -0500

    separating pipeline script

--- install.sh
+++ install.sh
@@ -4,3 +4,9 @@ echo 'set log_plays to true'
 export ANSIBLE_LOG_PATH=/var/log/ansible.log
 echo '*******************RUN PLAYBOOK****************'
 ansible-playbook -v -f 20 homework.yaml
+
+echo "Configure and Build Pipeline"
+chmod +x /root/shan-ocp-dep-hw-v2/scripts/cicd.sh
+sh /root/shan-ocp-dep-hw-v2/scripts/cicd.sh
+
+echo "Pipeline has been configured and first build has been started"
