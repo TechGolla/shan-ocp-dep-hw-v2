0e910a1e7b1b769c81e364247fbfb1dd5c11e575
TechGolla <techgolla@gmail.com>
2018-11-20 19:48:40 -0500

    pipeline changes - added prod env

--- scripts/cicd.sh
+++ scripts/cicd.sh
@@ -6,12 +6,16 @@ oc new-project dev --display-name="Tasks - Dev"
 echo "Create Stage project"
 oc new-project stage --display-name="Tasks - Stage"
 
+echo "Create Prod project"
+oc new-project prod --display-name="Tasks - Prod"
+
 echo "Create CI/CD project"
 oc new-project cicd --display-name="CI/CD"
 
-echo "Set serviceaccount status for CI/CD project for dev and stage projects"
+echo "Set serviceaccount status for CI/CD project for dev, stage and prod projects"
 oc policy add-role-to-group edit system:serviceaccounts:cicd -n dev
 oc policy add-role-to-group edit system:serviceaccounts:cicd -n stage
+oc policy add-role-to-group edit system:serviceaccounts:cicd -n prod
 
 echo "Start application deployment to trigger CI/CD workflow"
 oc new-app -n cicd -f /root/shan-ocp-dep-hw-v2/cicd_template.yaml
